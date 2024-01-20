05e0a039471fcbea59650f5b0bb2a4e8afba7ee6
ValentineWa <71629101+ValentineWa@users.noreply.github.com>
2024-01-20 15:06:33 +0300

    Add files via upload
    First commit - User prompts for name, generates unique ID, and enrolment.

---
+++ Student Database System/src/studentdatabaseapp/StudentDatabaseApp.java
@@ -0,0 +1,12 @@
+package studentdatabaseapp;
+
+public class StudentDatabaseApp {
+    public static void main (String [] args){
+
+        //Ask how many new users we want to add
+Student stu1 = new Student();
+stu1.enroll();
+
+        //Create n number of new students
+    }
+}
